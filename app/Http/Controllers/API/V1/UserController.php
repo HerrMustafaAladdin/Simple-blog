9a7f4c05bc24d037ca8a4d24fb2d0a620f948fda
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-30 09:03:40 +0330

    fixed index and store method

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers\API\V1;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\V1\ApiController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        // user model
+        $users = User::query()->orderBy('created_at')->paginate(10);
+
+        return $this->successResponce($users, '', 200);
     }
 
     /**
@@ -21,7 +26,29 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name'                      =>  ['required', 'string', 'min:3', 'max:100'],
+            'email'                     =>  ['required', 'email'],
+            'password'                  =>  ['required', 'string', 'min:6', 'max:32'],
+            'password_confirmation'     =>  ['required','same:password']
+        ]);
+
+        if($validator->fails())
+        {
+            return $this->errorResponce(422, $validator->messages());
+        }
+
+
+        $user = User::create([
+            'name'      =>  $request->input('name'),
+            'email'     =>  $request->input('email'),
+            'password'  =>  Hash::make($request->password),
+        ]);
+
+
+        return $this->successResponce($user, __('The new user was successfully created.'),200);
+
+
     }
 
     /**
